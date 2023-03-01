f2be455b874d97c8890f018515bb84522555b124
hjk810003 <hjk810003@gmail.com>
2023-03-01 17:14:54 +0900

    기본 구조 세팅

---
+++ src/components/common/Header/index.js
@@ -0,0 +1,9 @@
+import { Logo, Wrap } from "./styled"
+
+function Header() {
+  return (
+      <Wrap><Logo>로고</Logo></Wrap>
+  )
+}
+
+export default Header
---
+++ src/components/common/Header/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const Wrap = styled.header`
+  background: #f00;
+h1 {
+  color: #000;
+}
+`;
+
+export const Logo = styled.h1`
+  color: #000;
+`;
