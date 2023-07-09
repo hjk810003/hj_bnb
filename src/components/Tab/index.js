3a159a19efd359433f8dfe971025a1d407f87f19
hjk810003 <hjk810003@gmail.com>
2023-07-09 18:20:59 +0900

    contact 수정

--- src/components/Tab/index.js
+++ src/components/Tab/index.js
@@ -1,40 +1,39 @@
-import { useState } from 'react';
-import SwiperCore, { FreeMode } from 'swiper';
-import { TabSwiper, TabItem } from './styled';
-import { Inner } from 'styles/common';
+import { useState } from "react";
+import SwiperCore, { FreeMode } from "swiper";
+import { TabSwiper, TabItem } from "./styled";
+import { Inner } from "styles/common";
 
 SwiperCore.use([FreeMode]);
 
 function Tab({ items, label, onClick }) {
-	const [swiper, setSwiper] = useState(null);
+  const [swiper, setSwiper] = useState(null);
 
-	const params = {
-		onSwiper: setSwiper,
-		slidesPerView: 'auto',
-		freeMode: true,
-	};
+  const params = {
+    onSwiper: setSwiper,
+    slidesPerView: "auto",
+    freeMode: true,
+  };
 
-	const onClickHandler = (itemLabel) => {
-		onClick?.(itemLabel);
-		console.log(swiper?.clickedIndex);
-		return swiper?.slideTo(swiper?.clickedIndex, 300);
-	};
+  const onClickHandler = (itemLabel) => {
+    onClick?.(itemLabel);
+    return swiper?.slideTo(swiper?.clickedIndex, 300);
+  };
 
-	return (
-		<Inner>
-			<TabSwiper {...params}>
-				{items.map((item) => (
-					<TabItem
-						key={item.label}
-						onClick={() => onClickHandler(item.label)}
-						$selected={item.label === label}
-					>
-						{item?.icon} {item.name}
-					</TabItem>
-				))}
-			</TabSwiper>
-		</Inner>
-	);
+  return (
+    <Inner>
+      <TabSwiper {...params}>
+        {items.map((item) => (
+          <TabItem
+            key={item.label}
+            onClick={() => onClickHandler(item.label)}
+            $selected={item.label === label}
+          >
+            {item?.icon} {item.name}
+          </TabItem>
+        ))}
+      </TabSwiper>
+    </Inner>
+  );
 }
 
 export default Tab;
--- src/container/Mypage/components/Contact/index.js
+++ src/container/Mypage/components/Contact/index.js
@@ -1,84 +1,89 @@
-import axios from 'axios';
-import { useState } from 'react';
-import { ContactForm, Input, SubmitBtn, ButtonBox } from './styled';
+import axios from "axios";
+import { useState } from "react";
+import { ContactForm, Input, SubmitBtn, ButtonBox } from "./styled";
 
 function Contact() {
-	const [isLoading, setIsLoading] = useState(false);
-	// 인풋 데이터를 담을 state 생성
-	const [values, setValues] = useState({
-		name: '',
-		email: '',
-		message: '',
-	});
+  const [isLoading, setIsLoading] = useState(false);
+  // 인풋 데이터를 담을 state 생성
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-	const { name, email, message } = values;
+  const { name, email, message } = values;
 
-	// 인풋 change 이벤트
-	const onChange = (e) => {
-		const { name, value } = e.target;
-		setValues({
-			...values,
-			[name]: value,
-		});
-	};
+  // 인풋 change 이벤트
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
 
-	// 폼 전송(submit) 이벤트
-	const onSubmit = async (e) => {
-		e.preventDefault();
+  // 폼 전송(submit) 이벤트
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-		setIsLoading(true);
-		// javascript FormData
-		// https://ko.javascript.info/formdata
-		const formData = new FormData();
+    setIsLoading(true);
+    // javascript FormData
+    // https://ko.javascript.info/formdata
+    const formData = new FormData();
 
-		formData.append('name', name);
-		formData.append('email', email);
-		formData.append('message', message);
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("message", message);
 
-		try {
-			await axios.post(
-				'https://script.google.com/macros/s/AKfycbz1N3l5R-EXwnXhL9tqJ6xocjIMw-ewBymcyEy2pfKEn6nVA8shzldWYUL9J13RA0jQpw/exec',
-				formData,
-			);
-			setIsLoading(false);
-			console.log('성공!', values);
-		} catch (e) {
-			setIsLoading(false);
-			console.error(e);
-		}
-	};
+    try {
+      await axios.post(
+        "https://script.google.com/macros/s/AKfycbz1N3l5R-EXwnXhL9tqJ6xocjIMw-ewBymcyEy2pfKEn6nVA8shzldWYUL9J13RA0jQpw/exec",
+        formData
+      );
+      setIsLoading(false);
+      alert("전송에 성공하였습니다.");
+      setValues({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } catch (e) {
+      setIsLoading(false);
+      console.error(e);
+    }
+  };
 
-	if (isLoading) return <span>로딩중!!</span>;
+  if (isLoading) return <span>로딩중!!</span>;
 
-	return (
-		<ContactForm onSubmit={onSubmit}>
-			<Input
-				type='text'
-				value={name}
-				name='name'
-				placeholder='name'
-				onChange={onChange}
-			/>
-			<Input
-				type='text'
-				value={email}
-				onChange={onChange}
-				name='email'
-				placeholder='email'
-			/>
-			<Input
-				type='text'
-				value={message}
-				onChange={onChange}
-				name='message'
-				placeholder='message'
-			/>
+  return (
+    <ContactForm onSubmit={onSubmit}>
+      <Input
+        type="text"
+        value={name}
+        name="name"
+        placeholder="name"
+        onChange={onChange}
+      />
+      <Input
+        type="text"
+        value={email}
+        onChange={onChange}
+        name="email"
+        placeholder="email"
+      />
+      <Input
+        type="text"
+        value={message}
+        onChange={onChange}
+        name="message"
+        placeholder="message"
+      />
 
-			<ButtonBox>
-				<SubmitBtn type='submit'>전송</SubmitBtn>
-			</ButtonBox>
-		</ContactForm>
-	);
+      <ButtonBox>
+        <SubmitBtn type="submit">전송</SubmitBtn>
+      </ButtonBox>
+    </ContactForm>
+  );
 }
 
 export default Contact;
